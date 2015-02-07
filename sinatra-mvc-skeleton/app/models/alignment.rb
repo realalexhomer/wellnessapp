c0284c318b660fd9cdd58836534cba04de538655
realalexhomer <realalexhomer@gmail.com>
2015-02-07 15:38:14 -0500

    seeds alignments

--- sinatra-mvc-skeleton/app/models/alignment.rb
+++ sinatra-mvc-skeleton/app/models/alignment.rb
@@ -2,4 +2,7 @@ class Alignment < ActiveRecord::Base
   # Remember to create a migration!
   has_many :categories
   has_many :activities through: :categories
+
+  validates :name, uniqueness: {case_senstitive: false}
+
 end
---
+++ sinatra-mvc-skeleton/db/seeds.rb
@@ -0,0 +1,5 @@
+Alignment.create(name: "Body")
+Alignment.create(name: "Mind")
+Alignment.create(name: "Soul")
+
+#don't run this again!
